6b5e8b4c2b7d6beb3c55967df1fa4d39d1b98822
Katrina Pingryn <katep@indeed.com>
2024-02-17 13:58:49 -0600

    backend tracing

--- src/app.js
+++ src/app.js
@@ -4,9 +4,13 @@ const cors = require("cors");
 const articlesRouter = require("./articles/articles.router");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
+// const pinoHttp = require("pino-http");
+const logger = require("./config/logger");
 
 const app = express();
 
+// app.use(pinoHttp());
+app.use(logger);
 app.use(cors());
 app.use(express.json());
 
--- src/articles/articles.controller.js
+++ src/articles/articles.controller.js
@@ -2,20 +2,34 @@ const service = require("./articles.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 function hasData(req, res, next) {
+  const methodName = "hasData";
+  req.log.debug({ __filename, methodName, body: req.body });
   if (req.body.data) {
+    req.log.trace({ __filename, methodName, valid: true });
     return next();
   }
-  next({ status: 400, message: "body must have data property" });
+  // next({ status: 400, message: "body must have data property" });
+  const message = "body must have data property";
+  next({ status: 400, message: message });
+  req.log.trace({ __filename, methodName, valid: false }, message);
 }
 
 function dataHas(propertyName) {
-  return (request, response, next) => {
-    const { data = {} } = request.body;
+  // return (request, response, next) => {
+  //   const { data = {} } = request.body;
+  const methodName = `dataHas('${propertyName}')`;
+  return (req, res, next) => {
+    req.log.debug({ __filename, methodName, body: req.body });
+    const { data = {} } = req.body;
     const value = data[propertyName];
     if (value) {
+      req.log.trace({ __filename, methodName, valid: true });
       return next();
     }
-    next({ status: 400, message: `Article must include a ${propertyName}` });
+    // next({ status: 400, message: `Article must include a ${propertyName}` });
+    const message = `Article must include a ${propertyName}`;
+    next({ status: 400, message: message });
+    req.log.trace({ __filename, methodName, valid: false }, message);
   };
 }
 
@@ -31,10 +45,13 @@ async function create(req, res) {
 }
 
 async function list(req, res) {
+  const methodName = "list";
+  req.log.debug({ __filename, methodName });
   const data = await service.list();
   res.json({
     data,
   });
+  req.log.trace({ __filename, methodName, return: true, data });
 }
 
 module.exports = {
